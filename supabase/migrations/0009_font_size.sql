ff6f423ce2e553e890cbddc60cacf6c27b6e42ef
elias <elias@elias.com>
2026-07-14 17:35:28 +0300

    upload docker

--- supabase/migrations/0009_font_size.sql
+++
@@ -1,6 +0,0 @@
--- Migration 0009: Add font_size_scale to projects
--- Allows users to scale all text on slides proportionally
-
-ALTER TABLE projects
-  ADD COLUMN IF NOT EXISTS font_size_scale real NOT NULL DEFAULT 1.0
-    CHECK (font_size_scale BETWEEN 0.7 AND 1.5);
